2bfc1ee8429040cbf0c034c25682869987053b81
Ritik <ee3210704@iitd.ac.in>
2022-12-30 22:51:44 +0530

    4th

--- reg/forms.py
+++ reg/forms.py
@@ -6,5 +6,6 @@ from .models import Booking
 class BookKaro(ModelForm):
     class Meta:
         model=Booking
-        fields=('bookname','booknumber','bookuser','booksport','bookday','bookslot')
+        fields=['bookname','booknumber','bookuser','booksport','bookday','bookslot']
+        labels={'bookname':'Name','booknumber':'PhoneNo','bookuser':'Booking For','booksport':'Sport','bookday':'Day','bookslot':'Slot Time'}
          
