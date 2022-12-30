9842e0cfec1114503381bbac5a3121d8ef96060a
Ritik <ee3210704@iitd.ac.in>
2022-12-30 19:13:15 +0530

    3rd

--- mscomplex/settings.py
+++ mscomplex/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_**^4o5*!&)#w&uv$n6r!s(xx+!&#+_q(di9k7c$(uvu3fbg04'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
